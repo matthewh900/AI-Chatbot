77032e522a13f94d3192d6f1ca5b565100c1af19
matthewh900 <matthewh900@gmail.com>
2025-04-08 17:18:39 +0100

    add some functionality for google genai

---
+++ app/GeminiChat.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { Text, View } from "react-native";
+import { GoogleGenAI } from "@google/genai";
+
+export function GeminiChat() {
+    const [messages, setMessages] = useState([])
+    const [userInput, setUserInput] = useState<string>("")
+    const [loading, setLoading] = useState<boolean>(false)
+
+    const API_KEY = "AIzaSyAurfcK_crrwAWP9uoj2HtmASLlHJOXnAg"
+
+    useEffect(() => {
+        const startChat = async () => {
+            const ai = new GoogleGenAI({ apiKey: API_KEY })
+            const response = await ai.models.generateContent({
+                model: "gemini-2.0-flash",
+                contents: "Explain how AI works in a few words",
+              });
+            const text = response.text
+            // setMessages([
+            //     {
+            //         text,
+            //         user: false
+            //     },
+            // ])
+        }
+        startChat()
+    }, [])
+
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
