a0201ba9a61d319027b57884c24834eceb42ecf4
matthewh900 <matthewh900@gmail.com>
2025-04-09 17:23:02 +0100

    add functionality for chatbot with some styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { Text, View } from "react-native";
-import { GeminiChat } from "./GeminiChat";
+import { GeminiChat } from "../src/GeminiChat";
 
 export default function Index() {
   return (
